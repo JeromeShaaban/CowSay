c70c92a73819e5a690d0b86c19b619eb5593e2bc
JaddN7 <jerome.shaaban@gmail.com>
2021-03-25 11:57:20 +0100

    Const name updated in index.js ; alias start created; cowsay transfered in index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const greetings = require("./information.js");
+const studentInfo = require("./information.js");
+
+let cowsay = require("cowsay");
+
+console.log(cowsay.say({
+	text : `Hello, I'm ${studentInfo.studentName} from ${studentInfo.campus} !`,
+	e : "oO",
+	T : "U "
+}));
+
+// or cowsay.think()
--- information.js
+++ information.js
@@ -4,13 +4,3 @@ studentInfo = {
 }
 
 module.exports = studentInfo;
-
-let cowsay = require("cowsay");
-
-console.log(cowsay.say({
-	text : `Hello, I'm ${studentInfo.studentName} from ${studentInfo.campus} !`,
-	e : "oO",
-	T : "U "
-}));
-
-// or cowsay.think()
